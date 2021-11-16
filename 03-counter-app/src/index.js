ea8b7dfc26b6338f6308efbeaffcabd75c973afb
ALEX ENRIQUEZ <alexis.enriquezca@gmail.com>
2021-11-16 17:14:37 -0500

    Initial commit

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+import './index.css';
+
+const root = document.querySelector('#root');
+ReactDOM.render(<CounterApp value={10}/>, root);
---
+++ 03-counter-app/src/tests/demo.test.js
@@ -0,0 +1,9 @@
+
+
+test('should be true', () => {
+    const isActive = true;
+
+    if(isActive) {
+        throw new Error('Is not active');
+    }
+})
