08c3484f96887477699334366cf438880b66ef9b
ALEX ENRIQUEZ <alexis.enriquezca@gmail.com>
2021-11-18 09:52:02 -0500

    changing giphy url

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 export const getGifs = async( category ) => {
     const apiKey = 'NzfsgwuUHuPGCqsHgMW7sHKVmdNsTBmG';
     const limit = 10;
-    const giphyUrl = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=${limit}&api_key=${apiKey}`;
+    const giphyUrl = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=${limit}&api_key=${apiKey}`;
     
     const result = await fetch(giphyUrl)
     const { data } = await result.json();
