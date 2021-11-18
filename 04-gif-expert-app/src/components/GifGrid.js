991339c6ac7efb9a2b7ea702b00e46457d7c848d
ALEX ENRIQUEZ <alexis.enriquezca@gmail.com>
2021-11-18 09:54:18 -0500

    adding new release of docs

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -10,7 +10,7 @@ export const GifGrid = ({ category }) => {
         <>
         <h3>{ category }</h3>
 
-        { loading && <p>Loading</p> }
+        { loading && <p>Loading...</p> }
         <div className="card-grid">
             
             {
