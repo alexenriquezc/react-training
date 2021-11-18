dcfa577d92373ae528c6bfc0619e8ce0bcdfb312
ALEX ENRIQUEZ <alexis.enriquezca@gmail.com>
2021-11-17 22:21:59 -0500

    adding animate.css

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export const GifGridItem = ({ title, url }) => {
+    const rand = Math.random();
     return (
-        <div className="card">
+        <div className="card animate__animated animate__fadein" key={rand}>
             <img src={url} alt={title}/>
             <p>{title}</p>
         </div>
